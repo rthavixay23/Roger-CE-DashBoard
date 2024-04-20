71981a40776b6684dc541eb3a2741636dbb2db3a
rthavixay23 <rthavixay@ucsd.edu>
2024-04-20 16:27:09 -0700

    added new features and styling

--- my-dashboard/app/page.tsx
+++ my-dashboard/app/page.tsx
@@ -15,9 +15,19 @@ export default function Page() {
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Testing Web App Here.</strong>
+        <div className="flex flex-col flex items-center justify-center gap-6 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
+        <Image
+          src="/RGS-new-bird.png"
+          width={200}
+          height={200}
+          className="flex flex-row justify-content right align-items center"
+          alt="Screenshots of the dashboard project showing desktop version"
+          />  
+          <p className={` text-xl text-gray-800 md:text-3xl md:leading-normal text-center`}>
+          <div className="flex flex-wrap">
+  <p><strong className="mr-2">Roger's</strong><br/>Web App Here.</p>
+</div>
+            <strong></strong>
 
             {/* <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -35,9 +45,9 @@ export default function Page() {
           </p>
           <Link
             href="/dashboard"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-10 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            Log in <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
@@ -49,6 +59,8 @@ export default function Page() {
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
         />  
+
+
         </div>
       </div>
     </main>
