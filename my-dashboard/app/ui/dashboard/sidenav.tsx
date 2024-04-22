b473f4672f3e288875278946cdb4ca3c848be7f0
rthavixay23 <rthavixay@ucsd.edu>
2024-04-22 09:24:36 -0700

    test

--- my-dashboard/app/ui/dashboard/sidenav.tsx
+++ my-dashboard/app/ui/dashboard/sidenav.tsx
@@ -24,12 +24,12 @@ export default function SideNav() {
 
 
           {/**Sign Out Button */}
-          <NextLink href='/' passHref>
+
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-sky-400 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
-          </NextLink>
+
         </form>
       </div>
     </div>
