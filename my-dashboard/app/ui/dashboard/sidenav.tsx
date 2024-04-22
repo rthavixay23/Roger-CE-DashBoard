bd127c6528013f25b1bd7b44580522da84f88dd3
rthavixay23 <rthavixay@ucsd.edu>
2024-04-22 09:04:33 -0700

    Signout Button now returns to Login page

--- my-dashboard/app/ui/dashboard/sidenav.tsx
+++ my-dashboard/app/ui/dashboard/sidenav.tsx
@@ -1,9 +1,12 @@
+'use client'
+import NextLink from "next/link";
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -18,10 +21,15 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
+
+
+          {/**Sign Out Button */}
+          <NextLink href='/' passHref>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-sky-400 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
+          </NextLink>
         </form>
       </div>
     </div>
