dfe50b97d7da4024439af1ff69f75060d36e3661
rthavixay23 <rthavixay@ucsd.edu>
2024-04-23 16:05:02 -0700

    added latest invoices and recent revenue chart

--- my-dashboard/app/dashboard/page.tsx
+++ my-dashboard/app/dashboard/page.tsx
@@ -3,15 +3,20 @@
  * create a new route segment using a folder, and add a page file inside it.
 */
 
+//3 Component below ( Card, RevenueChart, and LatestInvoices) all recieves data.
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
  
-export default async function Page() {
+
+export default async function Page() { //This is an async component. Allows to use await to fetch data
+  const revenue = await fetchRevenue(); // To Fetch data for the RevenueChart component (receives data)
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
-      <h1>
+      <h1 className={` mb-4 text-xl md:text-2xl`}>
         Dashboard Page
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
@@ -25,8 +30,8 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
